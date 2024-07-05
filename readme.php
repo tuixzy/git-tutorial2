328d3aa119351044886780828800ab84c52ee1fd
Tui <6511130044@mut.ac.th>
2024-07-05 22:19:28 +0700

    update to remote

--- readme.php
+++ readme.php
@@ -1,6 +1,4 @@
-<?php 
-      echo "<h1>6511130044<h1>";
-      echo "<h1>Vatchara<h1>";
-      echo "<h1>VComputer Engineering<h1>";
-      echo "<h1>12 april 2004<h1>";
-      ?>
+<?php echo "6511130044"; ?>
+<?php echo "Vatchara Chanwong"; ?>
+<?php echo "Computer Engineering"; ?>
+  
